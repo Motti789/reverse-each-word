bae33f1dd7dd0d2c7751d1dc0abad979dc6f3a8c
Motti789 <mchillywilly@gmail.com>
2021-07-01 16:42:26 -0400

    commit tree 68686ba948b2e76565cd324439dbda9da919ef30

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
 require 'pry'
 def reverse_each_word(sentence)
-    sentence.split
-    sentence.each(&:reverse!)  do |line|
-        return "#{line}"
-end
+    array = sentence.split
+    maped = array.collect do |word|
+     word.reverse
+
+        # binding.pry
+    end
+    maped.join(" ")
 end
